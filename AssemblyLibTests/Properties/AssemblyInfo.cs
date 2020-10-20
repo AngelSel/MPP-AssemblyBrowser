1399b987898d80c31c0d5f4597aceeb637cd8a9c
AngelSel <angelbox1@gmail.com>
2020-10-20 11:53:59 +0300

    Project structure

---
+++ AssemblyLibTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyLibTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyLibTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e9ed8a3-bacd-420a-bcb1-792b17f6879b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AssemblyLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AssemblyLibrary
+{
+    public class Class1
+    {
+    }
+}
